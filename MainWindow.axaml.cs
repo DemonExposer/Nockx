3ba1d8292b9bdc04be99376423cab34a48799671
DemonExposer <kbdmns@gmail.com>
2024-05-12 15:22:01 +0200

    Created layout

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,9 +1,16 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
 namespace SecureChat;
 
 public partial class MainWindow : Window {
 	public MainWindow() {
 		InitializeComponent();
+	}
+
+	private void InitializeComponent() {
+		AvaloniaXamlLoader.Load(this);
+
+		StackPanel messagePanel = this.FindControl<StackPanel>("MessagePanel")!;
 	}
 }
