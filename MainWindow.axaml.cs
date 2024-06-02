334cadd02b4f04f5c7bf7776800c430476b2d11f
DemonExposer <kbdmns@gmail.com>
2024-06-02 17:11:56 +0200

    Implemented the "new chat" button and proper loading of ChatPanel

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,13 +1,12 @@
 using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using SecureChat.panels;
 
 namespace SecureChat;
 
 public partial class MainWindow : Window {
-	private DockPanel mainPanel;
+	private DockPanel _mainPanel;
+	private Panel? _uiPanel;
 
 	public MainWindow() => InitializeComponent();
 
@@ -17,20 +16,22 @@ public partial class MainWindow : Window {
 		AddUserPanel addUserPanel = (AddUserPanel) Resources["AddUserPanel"]!;
 		ChatPanel chatPanel = (ChatPanel) Resources["ChatPanel"]!;
 
-		mainPanel = this.FindControl<DockPanel>("MainPanel")!;
+		_mainPanel = this.FindControl<DockPanel>("MainPanel")!;
 
 		addUserPanel.SetOnEnter(publicKey => {
-			mainPanel.Children.Remove(addUserPanel);
-			mainPanel.Children.Add(chatPanel);
-
+			SetUiPanel(chatPanel);
 			chatPanel.Show("someone", publicKey);
 		});
 
-		mainPanel.Children.Add(addUserPanel);
-		this.FindControl<Button>("AddChatButton").Click += OnAddChatButtonClick;
+		SetUiPanel(addUserPanel);
+		this.FindControl<Button>("AddChatButton")!.Click += (_, _) => SetUiPanel(addUserPanel);
 	}
 
-	private void OnAddChatButtonClick(object sender, RoutedEventArgs e) {
+	private void SetUiPanel(Panel panel) {
+		if (_uiPanel != null)
+			_mainPanel.Children.Remove(_uiPanel);
 
+		_uiPanel = panel;
+		_mainPanel.Children.Add(panel);
 	}
 }
--- panels/ChatPanel.cs
+++ panels/ChatPanel.cs
@@ -1,50 +1,67 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.LogicalTree;
 using Org.BouncyCastle.Crypto.Parameters;
 using System.Collections.Generic;
+using Avalonia.Threading;
 
 namespace SecureChat.panels;
 public class ChatPanel : DockPanel {
-	private ChatPanelController _controller;
+	private readonly ChatPanelController _controller = new ();
+	private readonly List<TextBlock> _messages = new ();
+
+	private StackPanel? _messagePanel;
 
 	public ChatPanel() {
-		_controller = new ChatPanelController();
+		Dispatcher.UIThread.InvokeAsync(() => {
+			using IEnumerator<ILogical> enumerator = this.GetLogicalDescendants().GetEnumerator();
+			while (enumerator.MoveNext()) {
+				if (enumerator.Current.GetType() == typeof(TextBox)) {
+					TextBox textBox = (TextBox) enumerator.Current;
+					if (textBox.Name == "MessageBox") {
+						textBox.KeyDown += (_, args) => {
+							if (args.Key == Key.Enter) {
+								if (textBox.Text == null)
+									return;
+
+								_controller.Send(textBox.Text);
+							}
+						};
+					}
+				} else if (enumerator.Current.GetType() == typeof(StackPanel)) {
+					StackPanel stackPanel = (StackPanel) enumerator.Current;
+					if (stackPanel.Name == "MessagePanel")
+						_messagePanel = stackPanel;
+				}
+			}
+		});
 	}
 
 	public void Show(string username, RsaKeyParameters publicKey) {
+		// _messagePanel should never be null, because a user cannot open this panel before the UI is done.
+		// However, in theory, when the program is loaded from a saved state, it is theoretically possible to trigger this. So this is just for debug.
+		if (_messagePanel == null) {
+			Console.WriteLine("Show was called before initialization. Not displaying anything");
+			return;
+		}
+		
 		_controller.ForeignPublicKey = publicKey;
+		
+		_messagePanel.Children.RemoveAll(_messages);
+		_messages.Clear();
+					
+		string[] messages = _controller.GetPastMessages();
 
-		using IEnumerator<ILogical> enumerator = this.GetLogicalDescendants().GetEnumerator();
-		while (enumerator.MoveNext()) {
-			if (enumerator.Current.GetType() == typeof(TextBox)) {
-				TextBox textBox = (TextBox) enumerator.Current;
-				if (textBox.Name == "MessageBox") {
-					textBox.KeyDown += (_, args) => {
-						if (args.Key == Key.Enter) {
-							if (textBox.Text == null)
-								return;
-
-							_controller.Send(textBox.Text);
-						}
-					};
-				}
-			} else if (enumerator.Current.GetType() == typeof(StackPanel)) {
-				StackPanel stackPanel = (StackPanel) enumerator.Current;
-				if (stackPanel.Name == "MessagePanel") {
-					string[] messages = _controller.GetPastMessages();
-
-					foreach (string message in messages) {
-						string fullMessage = $"{username} | {message}";
-						TextBlock messageTextBlock = new () {
-							Text = fullMessage,
-							Margin = new Thickness(5)
-						};
-						stackPanel.Children.Add(messageTextBlock);
-					}
-				}
-			}
+		foreach (string message in messages) {
+			string fullMessage = $"{username} | {message}";
+			TextBlock messageTextBlock = new () {
+				Text = fullMessage,
+				Margin = new Thickness(5)
+			};
+			_messages.Add(messageTextBlock);
+			_messagePanel.Children.Add(messageTextBlock);
 		}
 	}
 }
