a97259c5a6ceee688d573dabf8d8b354f0f12164
DemonExposer <kbdmns@gmail.com>
2024-10-22 13:15:19 +0200

    Added code for decompressing auto-update zip file

--- windows/UpdateWindow.axaml.cs
+++ windows/UpdateWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.IO.Compression;
 using System.Net.Http;
 using System.Text.Json.Nodes;
 using System.Threading;
@@ -16,17 +18,27 @@ public partial class UpdateWindow : Window {
 	public async Task Update(JsonObject releaseFileObject) {
 		ProgressBar progressBar = this.FindControl<ProgressBar>("DownloadProgress")!;
 		progressBar.Maximum = releaseFileObject["size"]!.GetValue<long>();
-
-		// TODO: Add code to unpack the zip file
+		
 		await Task.Run(() => {
-			using HttpClient httpClient = new ();
-			using Task<Stream> streamTask = httpClient.GetStreamAsync(releaseFileObject["browser_download_url"]!.GetValue<string>());
-			using FileStream fileStream = new (releaseFileObject["name"]!.GetValue<string>(), FileMode.Create);
-			new Timer(_ => {
-				if (fileStream.CanRead)
-					Dispatcher.UIThread.InvokeAsync(() => progressBar.Value = fileStream.Position);
-			}, null, 0, 100);
-			streamTask.Result.CopyTo(fileStream);
+			try {
+				using HttpClient httpClient = new ();
+				using Task<Stream> streamTask = httpClient.GetStreamAsync(releaseFileObject["browser_download_url"]!.GetValue<string>());
+				using FileStream fileStream = new (releaseFileObject["name"]!.GetValue<string>(), FileMode.Create);
+				new Timer(_ => {
+					if (fileStream.CanRead)
+						Dispatcher.UIThread.InvokeAsync(() => progressBar.Value = fileStream.Position);
+				}, null, 0, 100);
+				streamTask.Result.CopyTo(fileStream);
+				fileStream.Position = 0;
+				ZipArchive zip = new (fileStream);
+				foreach (ZipArchiveEntry entry in zip.Entries) { // TODO: fix self-overwriting issue
+					using FileStream decompressedFileStream = new (entry.Name, FileMode.Create);
+					using Stream compressedFileStream = entry.Open();
+					compressedFileStream.CopyTo(decompressedFileStream);
+				}
+			} catch (Exception e) {
+				Console.WriteLine(e);
+			}
 		});
 	}
 }
