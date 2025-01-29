5151a4f749a5f422ad8c6c3fc61f1b1472657acc
Mart <78360535+MartMonster@users.noreply.github.com>
2025-01-29 20:49:54 +0100

    nicknames TODO: show in chat

--- SecureChat/panels/UserInfoPanel.cs
+++ SecureChat/panels/UserInfoPanel.cs
@@ -2,6 +2,7 @@
 using Avalonia.LogicalTree;
 using Avalonia.Threading;
 using System.Collections.Generic;
+using Avalonia.Input;
 
 namespace SecureChat.panels;
 
@@ -23,10 +24,20 @@ public class UserInfoPanel : StackPanel {
 						case "ExponentBox":
 							exponentBox = textBox;
 							break;
+						case "NicknameBox":
+							textBox.Text = _controller.Nickname;
+							textBox.KeyDown += (_, args) => {
+								if (args.Key == Key.Enter) {
+									if (textBox.Text == null)
+										return;
+									if(!_controller.SetNickname(textBox.Text))
+										textBox.Text = _controller.Nickname;
+								}
+							};
+							break;
 					}
 				}
 			}
-
 			modulusBox!.Text = _controller.PublicKey.Modulus.ToString(16);
 			exponentBox!.Text = _controller.PublicKey.Exponent.ToString(16);
 		});
--- SecureChat/panels/UserInfoPanelController.cs
+++ SecureChat/panels/UserInfoPanelController.cs
@@ -1,16 +1,52 @@
-﻿using Org.BouncyCastle.Crypto.Parameters;
+﻿using System;
+using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
 using SecureChat.util;
 using System.IO;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using LessAnnoyingHttp;
+using Org.BouncyCastle.Crypto;
 
 namespace SecureChat.panels;
 
 public class UserInfoPanelController {
 	public readonly RsaKeyParameters PublicKey;
+	private readonly RsaKeyParameters _privateKey;
+	private readonly Settings _settings = Settings.GetInstance();
+	public string Nickname = "";
 
 	public UserInfoPanelController() {
-		using StreamReader reader = File.OpenText(Constants.PublicKeyFile);
-		PemReader pemReader = new (reader);
-		PublicKey = (RsaKeyParameters) pemReader.ReadObject();
+		using (StreamReader reader = File.OpenText(Constants.PublicKeyFile)) {
+			PemReader pemReader = new(reader);
+			PublicKey = (RsaKeyParameters) pemReader.ReadObject();
+		}
+
+		using (StreamReader reader = File.OpenText(Constants.PrivateKeyFile)) {
+			PemReader pemReader = new (reader);
+			_privateKey = (RsaKeyParameters) ((AsymmetricCipherKeyPair) pemReader.ReadObject()).Private;
+		}
+		
+		GetNickname();
+	}
+
+	private void GetNickname() {
+		string getVariables = $"modulus={PublicKey.Modulus.ToString(16)}&exponent={PublicKey.Exponent.ToString(16)}";
+		Nickname = Http.Get($"http://{_settings.IpAddress}:5000/nickname?" + getVariables).Body;
+	}
+
+	public bool SetNickname(string nickname) {
+		JsonObject body = new() {
+			["modulus"] = PublicKey.Modulus.ToString(16),
+			["exponent"] = PublicKey.Exponent.ToString(16),
+			["nickname"] = nickname
+		};
+		string bodyString = JsonSerializer.Serialize(body);
+		Response response = Http.Put($"http://{_settings.IpAddress}:5000/nickname", bodyString, [new Header {Name = "Signature", Value = Cryptography.Sign(bodyString, _privateKey)}]);
+		if (response.IsSuccessful)
+			Nickname = nickname;
+		
+		return response.IsSuccessful;
 	}
 }
