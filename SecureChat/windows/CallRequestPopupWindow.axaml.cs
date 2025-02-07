fae3ced78fa3fe57fecce8d20f0ec5b2c3ae2d78
DemonExposer <56930597+DemonExposer@users.noreply.github.com>
2025-02-08 00:17:07 +0100

    Added functionality to Accept and Reject buttons for incoming call

--- SecureChat/windows/CallRequestPopupWindow.axaml.cs
+++ SecureChat/windows/CallRequestPopupWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Org.BouncyCastle.Crypto.Parameters;
+using SecureChat.audio;
 
 namespace SecureChat.windows;
 
@@ -18,5 +20,16 @@ public partial class CallRequestPopupWindow : PopupWindow {
 		AvaloniaXamlLoader.Load(this);
 		TextBlock nameTextBlock = this.FindControl<TextBlock>("CallerName")!;
 		nameTextBlock.Text = _foreignKey.Modulus.ToString(16); // TODO: change to display name
+	}
+
+	public void AcceptButton_OnClick(object? sender, RoutedEventArgs e) {
+		Sounds.Ringtone.StopRepeat();
+		new CallPopupWindow(_personalKey, _foreignKey).Show(MainWindow.Instance);
+		Close();
+	}
+
+	public void RejectButton_OnClick(object? sender, RoutedEventArgs e) {
+		Sounds.Ringtone.StopRepeat();
+		Close();
 	}
 }
