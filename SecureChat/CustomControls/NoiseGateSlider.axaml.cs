ea256a765519252588f2bbd4601d7aac82c28e36
DemonExposer <kbdmns@gmail.com>
2025-03-07 17:01:52 +0100

    Added ProgressBar property to NoiseGateSlider

--- SecureChat/CustomControls/NoiseGateSlider.axaml.cs
+++ SecureChat/CustomControls/NoiseGateSlider.axaml.cs
@@ -5,9 +5,15 @@ using Avalonia.Controls.Primitives;
 namespace SecureChat.CustomControls;
 
 public partial class NoiseGateSlider : UserControl {
+	protected readonly ProgressBar ProgressBar;
 	protected readonly Track Track;
 	
 	public double Value { get; protected set; }
+
+	public double VolumePercent {
+		get => ProgressBar.Value;
+		set => ProgressBar.Value = value;
+	}
 	
 	public event EventHandler<RangeBaseValueChangedEventArgs>? ValueChanged {
 		add => AddHandler(RangeBase.ValueChangedEvent, value);
@@ -17,7 +23,9 @@ public partial class NoiseGateSlider : UserControl {
 	public NoiseGateSlider() {
 		InitializeComponent();
 		
+		ProgressBar = this.FindControl<ProgressBar>("VolumeBar")!;
 		Track = this.FindControl<Track>("PART_Track")!;
+		
 		Value = Track.Value;
 		Track.Thumb!.DragDelta += (_, _) => {
 			RaiseEvent(new RangeBaseValueChangedEventArgs(Value, Track.Value, RangeBase.ValueChangedEvent, this));
